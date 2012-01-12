5f8de344c4587caa7388946b812a274096b9d4c6
Karl Hiner <karl.hiner@gmail.com>
2012-01-11 23:48:08 -0800

    Refactoring - now storing weight vectors of every class in a global dictionary.

--- perceptron.py
+++ perceptron.py
@@ -5,74 +5,72 @@ learning_rate = .2
 num_features = None
 train_lines = []
 test_lines = []
+weights = {}
 
 def main(trainfile_string, testfile_string):
     global train_lines
     global test_lines
     global num_features
+    global weights
     train_lines = open(trainfile_string, 'r').readlines()
     test_lines = open(testfile_string, 'r').readlines()    
     num_features = len(train_lines[0].strip().split(',')) - 1
-    
-    # make a pass through the data once for each number other than 8
-    # and classify n vs 8
+
+    # initialize weight vectors to random values for each class.
+    #!! To generalize to any classes,
+    #!! this is the only code that needs to change
     for n in xrange(10):
         if n != 8:
-            test(n, getWeights(n))
-
+            # start with random weights in range (-1, 1)            
+            weights[n] = [random.uniform(-1,1) for i in xrange(num_features)]
+    for cls in weights.keys():
+        train(cls)
+        test(cls)
             
-def getWeights(num):
+def train(cls):
     "Train perceptron to differentiate between num and 8, \
     and return the trained weights weights"
     global learning_rate
     global num_features
     global train_lines
-    # start with random weights in range (-1, 1)
-    weights = [random.uniform(-1,1) for i in xrange(num_features)]
+
     for line in train_lines:
         # store a line of data as a list of ints
         data = [int(x) for x in line.strip().split(',')]
-        t = getT(num, data)
-        if t == 0:
-            continue
-        o = getO(data, weights)
+        t = getT(data[-1])
+        o = getO(data, cls)
         # adjust the weights
         for i in xrange(num_features):
-            weights[i] += learning_rate*(t - o)*data[i]
+            weights[cls][i] += learning_rate*(t - o)*data[i]
             
-    return weights
     
-def test(num, weights):
+def test(cls):
     global test_lines
     p = 0;
     n = 0;
     for line in test_lines:
         # store a line of data as a list of ints
         data = [int(x) for x in line.strip().split(',')]
-        t = getT(num, data)
-        if t == 0:
-            continue
-        o = getO(data, weights)
+        t = getT(data[-1])
+        o = getO(data, cls)
         if (o == t):
             p = p + 1
         else:
             n = n + 1
     print('Success Rate = %f\n' % (float(p)/float(n + p)))
 
-def getT(num, data):
+def getT(cls):
         # expected value must be -1 or 1    
-        if data[-1] == 8:
+        if cls == 8:
             return 1
-        elif data[-1] == num:
-            return -1
         else:
-            return 0
+            return -1
 
-def getO(data, weights):
+def getO(data, cls):
     "Get o value"
     total = 0.0
     for i in xrange(num_features):
-        total += weights[i]*data[i]
+        total += weights[cls][i]*data[i]
     return sgn(total)
         
 def sgn(val):
